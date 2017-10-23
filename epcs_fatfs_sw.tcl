14ee6a2447d3eb19f7123dee14aa74a341003f32
kimu_shu <alfvegardrisc@gmail.com>
2017-10-24 01:15:01 +0900

    Add LFN and codepage option

--- epcs_fatfs_sw.tcl
+++ epcs_fatfs_sw.tcl
@@ -5,18 +5,24 @@
 
 create_sw_package epcs_fatfs
 
-set_sw_property version 1.1
+set_sw_property version 1.2
 
 set_sw_property auto_initialize true
 set_sw_property bsp_subdirectory drivers
 
 # Source files
 add_sw_property c_source fatfs/src/ff.c
+add_sw_property c_source fatfs/src/option/unicode.c
 add_sw_property c_source src/epcs_fatfs.c
 add_sw_property c_source src/epcs_fatfs_diskio.c
 add_sw_property include_source fatfs/src/diskio.h
 add_sw_property include_source fatfs/src/ff.h
 add_sw_property include_source fatfs/src/integer.h
+add_sw_property include_source fatfs/src/option/cc932.h
+add_sw_property include_source fatfs/src/option/cc936.h
+add_sw_property include_source fatfs/src/option/cc949.h
+add_sw_property include_source fatfs/src/option/cc950.h
+add_sw_property include_source fatfs/src/option/ccsbcs.h
 add_sw_property include_source inc/epcs_fatfs.h
 add_sw_property include_source inc/ffconf.h
 add_sw_property include_directory fatfs/src
@@ -39,6 +45,8 @@ add_sw_setting hex_number system_h_define flash.start_address EPCS_FATFS_FLASH_S
 add_sw_setting hex_number system_h_define flash.end_address EPCS_FATFS_FLASH_END 0x0 "End address of flash (0=auto detect)"
 add_sw_setting hex_number system_h_define flash.sector_erase_cmd EPCS_FATFS_FLASH_CMD_ERASE 0x20 "Command for sector erase"
 add_sw_setting boolean_define_only system_h_define flash.enable_verify EPCS_FATFS_FLASH_VERIFY 0 "Verify after writing sector"
+add_sw_setting boolean system_h_define fatfs.use_lfn EPCS_FATFS_USE_LFN 1 "Use LFN feature"
+add_sw_setting decimal_number system_h_define fatfs.code_page EPCS_FATFS_CODE_PAGE 1 "Code page (1:ASCII, 437:US, 932:Japanese, etc)"
 # add_sw_setting boolean_define_only system_h_define epcs.omit_erase EPCS_FATFS_FLASH_OMIT_ERASE 0 "Omit erase if erase is not required"
 
 # End of file
